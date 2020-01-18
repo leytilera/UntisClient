6e06cc4c0839ba9e82f09af2a998fb038e1f71b6
Timo Ley <timo@leyt.xyz>
2020-01-18 16:44:12 +0100

    Add Constants

---
+++ src/main/java/ley/untis/data/Constants.java
@@ -0,0 +1,19 @@
+package ley.untis.data;
+
+public class Constants {
+
+    public static final int SUNDAY = 1;
+    public static final int MONDAY = 2;
+    public static final int TUESDAY = 3;
+    public static final int WEDNESDAY = 4;
+    public static final int THURSDAY = 5;
+    public static final int FRIDAY = 6;
+    public static final int SATURDAY = 7;
+
+    public static final int KLASSE = 1;
+    public static final int TEACHER = 2;
+    public static final int ROOM = 3;
+    public static final int SUBJECT = 4;
+    public static final int STUDENT = 5;
+
+}
