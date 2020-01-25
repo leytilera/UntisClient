5f33782a3f042c2e952a449d6ad0cf6529964751
Timo Ley <timo@leyt.xyz>
2020-01-25 15:38:50 +0100

    Improved Exception system

--- src/main/java/ley/untis/exception/APIRequestException.java
+++ src/main/java/ley/untis/exception/APIRequestException.java
@@ -1,16 +1,47 @@
 package ley.untis.exception;
 
+import org.omg.CORBA.UNKNOWN;
+
 public class APIRequestException extends Exception {
 
     Throwable source;
+    String message;
+    EnumError errorType;
 
     public APIRequestException(Throwable error) {
         source = error;
+        message = source.getMessage();
+        if (message == null) {
+            errorType = EnumError.UNKNOWN;
+        } else if (message.contains("no such element")) {
+            errorType = EnumError.ELEMENTNOTFOUND;
+        } else if (message.contains("no right")) {
+            errorType = EnumError.NOPERMISSION;
+        } else {
+            switch (message) {
+                case "bad credentials":
+                    errorType = EnumError.BADCREDENTIALS;
+                    break;
+                case "0":
+                    errorType = EnumError.SCHOOLNOTFOUND;
+                    break;
+                case "too many results":
+                    errorType = EnumError.TOOMANYSCHOOLS;
+                    break;
+                default:
+                    errorType = EnumError.UNKNOWN;
+                    break;
+            }
+        }
+    }
+
+    public EnumError getError() {
+        return errorType;
     }
 
     @Override
     public String getMessage() {
-        return source.getMessage();
+        return message;
     }
 
     @Override
---
+++ src/main/java/ley/untis/exception/EnumError.java
@@ -0,0 +1,13 @@
+package ley.untis.exception;
+
+public enum EnumError {
+
+    UNKNOWN,
+    BADCREDENTIALS,
+    SCHOOLNOTFOUND,
+    TOOMANYSCHOOLS,
+    NOCONNECTION,
+    NOPERMISSION,
+    ELEMENTNOTFOUND
+
+}
